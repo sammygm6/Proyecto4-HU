b2a8c12a23b03934d16a52ad16dc941698eea1f8
Sammy Guevara <sammygm6@gmail.com>
2015-09-19 21:36:31 -0600

    Creando una clase Nodo MyNode con un atributo int id

---
+++ src/proyecto4/hu/MyNode.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyecto4.hu;
+
+/**
+ *
+ * @author User
+ */
+public class MyNode {
+    private int id;
+
+    public MyNode() {
+    }
+
+    public MyNode(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
